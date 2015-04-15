e70496ccf52ef46fa95f20e229b5425c8c3e2e31
Tom Livesey <tom@Toms-MacBook.local>
2015-04-15 13:23:09 +0100

    styling

--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -1,2 +1,9 @@
 module LinksHelper
+	
+	def date_change?(posts, current_index)
+		if current_index == 0 || posts[current_index].posted_at.day != posts[current_index-1].posted_at.day
+			true
+		end
+	end
+
 end
